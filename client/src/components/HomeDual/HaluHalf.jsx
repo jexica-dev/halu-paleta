d7de29b5b3dbf78c06983ad90d6ec3b3be66d757
Jexica Ayran <jexica@jexica-mac.myfiosgateway.com>
2022-09-15 17:00:12 -0400

    p5js tests

--- client/src/components/HomeDual/HaluHalf.jsx
+++ client/src/components/HomeDual/HaluHalf.jsx
@@ -6,13 +6,18 @@ import sketch from "../P5/sketchHalu.js";
 
 export default function HaluHalf() {
   return (
-    <div className="star-cursor overflow-hidden w-full h-full relative bg-yellow">
-      <ReactP5Wrapper sketch={sketch} />
-      <img
+    <>
+      <div className="star-cursor overflow-hidden w-full h-full relative bg-yellow">
+        <ReactP5Wrapper
+          className=" z-50 -mt-50 bg-transparent"
+          sketch={sketch}
+        />
+      </div>
+      {/* <img
         src={halohalo}
         alt="halo halo ice cream"
-        className=" absolute bottom-0 left-0 -ml-16 -mb-20 w-5/6"
-      />
-    </div>
+        className="z-10 absolute bottom-0 left-0 -ml-16 -mb-20 w-5/6"
+      /> */}
+    </>
   );
 }
--- client/src/components/P5/sketchHalu.js
+++ client/src/components/P5/sketchHalu.js
@@ -1,16 +1,29 @@
 function sketch(p) {
   p.setup = function () {
     p.createCanvas(1000, 1000);
+    p.img = p.loadImage("https://i.imgur.com/jCs31K2.png");
   };
 
   p.draw = function () {
-    if (p.mouseIsPressed) {
-      p.fill(0);
-    } else {
-      p.fill(255);
+    if (!p.mouseIsPressed) {
+      p.fill(255, 0, 0);
+      p.strokeWeight(0.25);
     }
+    // else {
+    //   p.fill(255, 0, 0);
+    // }
 
-    p.ellipse(p.mouseX, p.mouseY, 80, 80);
+    p.triangle(
+      p.mouseX + 30 - 30,
+      p.mouseY + 75 - 60,
+      p.mouseX + 58 - 30,
+      p.mouseY + 20 - 60,
+      p.mouseX + 86 - 30,
+      p.mouseY + 75 - 60
+    );
+
+    p.image(p.img, 0, p.height / 2, p.img.width / 2, p.img.height / 2);
+    // p.ellipse(p.mouseX, p.mouseY, 80, 80);
   };
 }
 
